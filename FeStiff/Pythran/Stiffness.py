c80921b82bedcac4bdfb600970f6bcd0912359aa
Thierry Dumont <tdumont@math.univ-lyon1.fr>
2018-05-10 10:47:23 +0200

    FeStiff/Pythran solved
    (Pb with types in array gq)

--- FeStiff/Pythran/Stiffness.py
+++ FeStiff/Pythran/Stiffness.py
@@ -1,13 +1,15 @@
 import numpy as np
 #from StiffOut import *
-import StiffOut as S
+import StiffOut as Sout
 class Stiffness:
     def __init__(self):
         self.ji=np.empty(4)
         self.grads=np.empty(36)
-        self.gq=np.array([-1, -1, 1, 1, -1, -1, 1, 0, 1, 0, -1, 0, 0, -1,
-                          0, 1, 0, 1, 0, -2, -2, -2, 2, 0, 0, 2, 2, 2, 2,
-                          0, 0, 2, -2, -2, -2, 0])
+        self.gq=np.array([-1., -1., 1., 1., -1., -1., 1., 0., 1., 0.,
+                          -1., 0., 0., -1.,
+                          0., 1., 0., 1., 0., -2., -2., -2., 2., 0., 0.,
+                          2., 2., 2., 2.,
+                          0., 0., 2., -2., -2., -2., 0])
 
     def op(self,x,y,m):
-        S.StiffOut(x,y,m,self.ji,self.grads,self.gq)
+        Sout.StiffOut(x,y,m,self.ji,self.grads,self.gq)
