55cd890d3b1c5fad2d6542a1311276c2054f14de
Thierry Dumont <tdumont@math.univ-lyon1.fr>
2018-05-10 10:52:22 +0200

    FeStiff/Pythran ok
    Error was caused by incorrect typing of an array.

--- FeStiff/Pythran/Stiffness.py
+++ FeStiff/Pythran/Stiffness.py
@@ -11,5 +11,13 @@ class Stiffness:
                           2., 2., 2., 2.,
                           0., 0., 2., -2., -2., -2., 0])
 
+        #this causes an error "Invalid call to pythranized function",
+        #probably because the array is seen as an arary of integers.
+        # self.gq=np.array([-1, -1, 1, 1, -1, -1, 1, 0, 1, 0,
+        #                   -1, 0, 0, -1,
+        #                   0, 1, 0, 1, 0, -2, -2, -2, 2, 0, 0,
+        #                   2, 2, 2, 2,
+        #                   0, 0, 2, -2, -2, -2, 0])
+
     def op(self,x,y,m):
         Sout.StiffOut(x,y,m,self.ji,self.grads,self.gq)
