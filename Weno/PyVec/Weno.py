192ede7a55e062e1723fdb5e06564c0563a6cd01
Roland Denis <roland.denis@math.univ-lyon1.fr>
2018-05-30 16:36:01 +0200

    Documenting vectorized Python version of WENO.

--- Weno/PyVec/Weno.py
+++ Weno/PyVec/Weno.py
@@ -5,7 +5,8 @@ class Weno:
         self.epsilon = 1e-6
 
     def weno(self, NumFl, Fl, L, In, Out):
-        
+        """ Interface reconstruction using WENO scheme. """
+
         # Built an extenday array with phantom cells to deal with periodicity
         #data = np.concatenate((In[-2:], In, In[0:2]))
         data = np.concatenate((In[-3:], In, In[0:2])) # FIXME: shift indexes so that there is no need to add 3 points at the left side
@@ -21,7 +22,7 @@ class Weno:
         S2 = 13./12. * dp1m2p1 + 1./4. * dp1p0m1
         S3 = 13./12. * dp1m2p1 + 1./4. * dp3m4p1
 
-        # Unweighted coefficients
+        # Unweighted coefficients that can be reused for right reconstruction
         w1_tmp = np.reciprocal(np.square(self.epsilon + S1))
         w2_tmp = np.reciprocal(np.square(self.epsilon + S2))
         w3_tmp = np.reciprocal(np.square(self.epsilon + S3))
@@ -42,6 +43,16 @@ class Weno:
         # Left reconstruction
         qm = np.divide(w1 * ip2m7p11[0:-3] + w2 * im1p5p2[1:-2] + w3 * ip2p5m1[2:-1], atot);
 
+        # Note: for the right reconstruction, the input is processed like
+        #   its order is reversed: for example, instead of dp1m4p3, we use
+        #   dp3m4p1. But in fact, due to some symmetries, the main work has
+        #   already be done during the left reconstruction.
+        
+        # Regularity indicators
+        # S1 = 13./12. * dp1m2p1 + 1./4. * dp3m4p1 # == S3 from the first step
+        # S2 = 13./12. * dp1m2p1 + 1./4. * dp1p0m1 # == S2 from the first step
+        # S3 = 13./12. * dp1m2p1 + 1./4. * dp1m4p3 # == S1 from the first step
+
         # Weighted coefficients for right reconstruction
         w1 = 1./10. * w3_tmp[3:]
         w2 = 3./5. * w2_tmp[2:-1]
@@ -52,6 +63,8 @@ class Weno:
 
         # Interpolations
         ip11m7p2 = np.convolve(data, np.flipud([11./6., -7./6., 2./6.]), 'valid')
+        # ip2p5m1 = np.convolve(data, np.flipud([2./6., 5./6., -1./6.]), 'valid') # Already calculated
+        # im1p5p2 = np.convolve(data, np.flipud([-1./6., 5./6., 2./6.]), 'valid') # Already calculated
 
         # Right reconstruction
         qp = np.divide(w1 * ip11m7p2[3:] + w2 * ip2p5m1[2:-1] + w3 * im1p5p2[1:-2], atot);
