9387dc371bef8c8d5b24429d986007b62a638773
Thierry Dumont <tdumont@math.univ-lyon1.fr>
2018-06-03 20:08:42 +0200

    cosmetic!

--- MicroBenchmarks/C++/get_time.hpp
+++ MicroBenchmarks/C++/get_time.hpp
@@ -2,8 +2,5 @@
 #include <sys/time.h>
 #include <chrono>
 auto  get_time() {
-    // struct timeval tv;
-    // gettimeofday(&tv,0);
-    // return (double) tv.tv_sec+tv.tv_usec*1e-6;
   return std::chrono::high_resolution_clock::now();
 }
