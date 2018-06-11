9720697fd226f4045f4dce47dab7be2a3526a315
Benoit Fabrèges <fabreges@math.univ-lyon1.fr>
2018-06-11 15:18:12 +0200

    Fortran implementation of Weno.

---
+++ Weno/Fortran/m_problem.F90
@@ -0,0 +1,6 @@
+module m_problem
+
+  use m_burghers
+  use m_godunov
+  
+end module m_problem
---
+++ Weno/Fortran/main.F90
@@ -0,0 +1,66 @@
+program main
+
+  use m_weno
+  use m_RK3TVDData
+  
+  implicit none
+
+  integer, parameter :: size = 1000
+  real(kind = 8), parameter :: L = 1.
+  real(kind = 8), parameter :: T = 1.
+  real(kind = 8), parameter :: dt = 0.8 / size
+  
+  real(kind = 8) :: tt, start, finish
+
+  real(kind = 8), dimension(size) :: In, Out, Temp
+
+  type(WenoData) :: W
+  type(RK3TVDData) :: R
+  
+  print *, size, dt, floor(T/dt)
+
+  call init(In, L, size)
+  call initWenoData(W, size)
+  call initRK3TVDData(R, size)
+
+  print *, "Start computation"
+
+  tt = 0.
+  call cpu_time(start)
+  do while(tt < T)
+     call Rk3tvd(R, W, L, dt, In, Out)
+     Temp = In
+     In = Out
+     Out = Temp
+
+     tt = tt + dt
+  end do
+  call cpu_time(finish)
+
+  print *, "Time : ", finish - start
+  
+end program main
+
+
+subroutine init(X, L, len)
+
+  implicit none
+
+  integer, intent(in) :: len
+  real(kind = 8), intent(in) :: L
+  real(kind = 8), dimension(len), intent(inout) :: X
+
+  real(kind = 8) :: h
+  integer :: i
+
+  h = L / len
+
+  do i = 0, len-1
+     if(i > floor(len / 8.) .and. i < floor(len / 2.) + floor(len / 8.)) then
+        X(i+1) = 1. - 2. * (i - floor(len / 8.)) * h / L
+     else
+        X(i+1) = 0.
+     end if
+  end do
+     
+end subroutine init
